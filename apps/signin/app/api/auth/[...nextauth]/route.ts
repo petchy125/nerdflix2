58402e84c363d94dc6208a7b0caf5981db043c29
Amornwut Lertsiriumnouyphon <petchy125@hotmail.com>
2023-10-01 16:17:32 +0700

    move all pages to nerdflix2

---
+++ apps/signin/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from 'next-auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { auth } from '@nerdflix-nx-cypress/shared/utils/firebase';
+
+type Credential = {
+  email: string;
+  password: string;
+};
+
+export const authOptions = {
+  pages: {
+    signIn: '/signin',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {},
+      async authorize(credentials): Promise<any> {
+        try {
+          const userCredential = await signInWithEmailAndPassword(
+            auth,
+            (credentials as unknown as Credential).email || '',
+            (credentials as unknown as Credential).password || ''
+          );
+
+          if (userCredential.user) {
+            return userCredential.user;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
